1de4570602cf646d8e5dad751929216cd9c76bec
Gaetano Pessa <g.pessa@goldrepublic.com>
2020-07-19 21:54:02 +0200

    deploy to github pages

--- vue.config.ts
+++ vue.config.ts
@@ -1,9 +1,11 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/test-8/'
+    : '/',
   chainWebpack: (config) => {
+    // Svg
     const svgRule = config.module.rule('svg');
-
     svgRule.uses.clear();
-
     svgRule
       .use('babel-loader')
       .loader('babel-loader')
